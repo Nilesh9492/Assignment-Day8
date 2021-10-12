f8e8fcb9ee25712b654a672c9491359807611065
Nilesh9492 <nilesh94041@gmail.com>
2021-10-12 12:23:48 +0530

    first commit

---
+++ LineComparison/LineComparison/LengthCalculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LineComparison
+{
+    class LengthCalculation
+    {
+        public double Findlength(double x1, double x2, double y1,double y2)
+        {
+
+
+            
+            double Distance;
+            
+            Distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+            Console.WriteLine("Line length is " + Distance);
+            return Distance;
+        }
+
+    }
+}
---
+++ LineComparison/LineComparison/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LengthCalculation length = new LengthCalculation();
+            length.Findlength(2, 20, 2, 20);
+        }
+    }
+}
