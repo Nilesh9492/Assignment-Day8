1dedfde744e779f84f47d69a3422f0cf1e6bd3c4
Nilesh9492 <nilesh94041@gmail.com>
2021-10-12 16:29:53 +0530

    second commit

---
+++ EmployeeWage-OOP/EmployeeWage-OOP/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EmployeeWage_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmployeeWage employeeWage = new EmployeeWage();
+            employeeWage.CalculateEmployeeAttendance();
+            employeeWage.CalculateDaillyEmpWage();
+            employeeWage.PartTimeWage();
+            employeeWage.EmpWageinswitch();
+            employeeWage.EmpWagePerMonth();
+            employeeWage.EmpWageinCondition();
+            EmployeeWage.CalculationEmpWage();
+        }
+    }
+}
