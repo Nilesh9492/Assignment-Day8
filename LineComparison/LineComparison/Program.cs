d046fed5ccf56a2fab99174fce4323b1afbaa464
Nilesh9492 <nilesh94041@gmail.com>
2021-10-12 12:35:44 +0530

    firs commit

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -7,7 +7,7 @@ namespace LineComparison
         static void Main(string[] args)
         {
             LengthCalculation length = new LengthCalculation();
-            length.Findlength(2, 20, 2, 20);
+            length.Findlength(4,14,4,21);
         }
     }
 }
