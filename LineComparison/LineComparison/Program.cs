41805e7e0e7dc1d46525691568ea39b24d8dbcdd
Nilesh9492 <nilesh94041@gmail.com>
2021-10-12 12:40:44 +0530

    second commit

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -7,7 +7,19 @@ namespace LineComparison
         static void Main(string[] args)
         {
             LengthCalculation length = new LengthCalculation();
-            length.Findlength(5,23,9,35);
+            double FirstLine = length.Findlength(3,12,4,15);
+            double Secondline = length.Findlength(4,6,3,5);
+
+            Boolean check = FirstLine.Equals(Secondline);
+            if (check == true)
+            {
+                Console.WriteLine("Lines are of equal length");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equal in length");
+            }
+
         }
     }
 }
